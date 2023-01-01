da812f8b2594d2b6b28a2e7e21349664ebadecf1
Christian Deacon <gamemann@gflclan.com>
2023-01-01 10:04:16 -0600

    Add helper function to retrieve file type of base64 string.

---
+++ src/utils/base64.ts
@@ -0,0 +1,37 @@
+const FileType = (str: string): string => {
+    let fileType = 'unknown';
+
+    switch (str[0]) {
+        case '/':
+            fileType = 'jpeg';
+
+            break;
+
+        case 'i':
+            fileType = 'png';
+
+            break;
+
+        case 'R':
+            fileType = 'gif';
+
+            break;
+
+        case 'U':
+            fileType = 'webp';
+
+            break;
+
+        // Not supporting PDF for now.
+        /*
+        case 'J':
+            fileType = 'pdf';
+
+            break;
+        */
+    }
+
+    return fileType;
+}
+
+export default FileType;
