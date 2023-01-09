166dd477f7aa3eee3bc087e4d3811011b6577709
Christian Deacon <gamemann@gflclan.com>
2023-01-08 18:48:54 -0600

    Make progress on create category.

--- src/components/forms/contributor/CreateCategory.tsx
+++ src/components/forms/contributor/CreateCategory.tsx
@@ -1,28 +1,180 @@
-import { Mod } from "@prisma/client";
-import { useFormik } from "formik";
-import React, { useState } from "react";
+
+import { useFormik, FormikProvider, Field } from "formik";
+import React, { useState, useEffect } from "react";
 
 import { trpc } from "../../../utils/trpc";
 
-const CategoryForm: React.FC<{id: Number | null}> = ({ id }) => {
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+const CategoryForm: React.FC<{id: number | null}> = ({ id }) => {
     const [icon, setIcon] = useState<File | null>(null);
 
+    // For editing (prefilled fields).
+    let parent_id = 0;
+    let name = "";
+    let name_short = "";
+    let url = "";
+    let classes = "";
+
+    let iconData: string | ArrayBuffer | null = null;
+
+    const categoryMut = trpc.category.addCategory.useMutation();
+    const categoryParents = trpc.category.getCategoriesByParent.useQuery();
+
+    useEffect(() => {
+        // Check if we have an error.
+        if (categoryMut.isError) {
+            let errMsg = "";
+
+            // Check if we can simplify the error message for client.
+            if (categoryMut.error.message.includes("file extension is unknown"))
+                errMsg = categoryMut.error.message;
+            else if (categoryMut.error.message.includes("base64 data is null"))
+                errMsg = "Icon file corrupt/invalid.";
+             else
+                errMsg = "Unable to create or edit category!"; 
+
+            // Send alert and log full error to client's console.
+            console.error(categoryMut.error);
+            alert("Error! " + errMsg);
+        }
+    }, [categoryMut.isError]);
+
+    // If we have a pre URL, that must mean we're editing. Therefore, pull existing category data.
+    if (id != null) {
+        // Retrieve category.
+        const categoryQuery = trpc.category.getCategory.useQuery({id: id});
+        const category = categoryQuery.data;
+
+        // Check if our category is null.
+        if (category != null) {
+            name = category.name;
+            name_short = category.name_short;
+            parent_id = category.parentId;
+            url = category.url;
+
+            // Classes is optional; Check if null.
+            if (category.classes != null)
+                classes = category.classes;
+        }
+    }
+
+    // Create form using Formik.
     const form = useFormik({
         initialValues: {
-            parent: 0,
-            name: "Category Name",
-            name_short: "Category Short Name",
-            classes: ""
+            name: name,
+            name_short: name_short,
+            parent_id: parent_id,
+            url: url,
+            classes: classes,
+            iremove: false
         },
+        enableReinitialize: true,
 
         onSubmit: (values) => {
+            // First, handle file uploads via a promise. Not sure of any other way to do it at the moment (though I am new to TypeScript, Next.JS, and React).
+            new Promise<void>(async (resolve, reject) => {
+                // We have uploads / total uploads.
+                let uploads: number = 0;
+                let totalUploads: number = 0;
+                
+                // Check icon and handle upload.
+                if (icon != null) {
+                    // Increase our total uploads count.
+                    totalUploads++;
+        
+                    // Create new reader.
+                    const reader = new FileReader();
+        
+                    // On file uploaded.
+                    reader.onload = () => {
+                        console.debug("Icon uploaded!");
+        
+                        // Set Base64 data to iconData.
+                        iconData = reader.result;
+        
+                        console.debug("Icon data => " + iconData);
+        
+                        // We're done; Increment uploads.
+                        uploads++;
+                    };
+        
+                    // Read icon file.
+                    reader.readAsDataURL(icon);
+                }
+                
+                // Create a for loop for 30 seconds to allow files to upload. We could make a while loop, but I'd prefer having a 30 second timeout (these are image files).
+                for (let i = 0; i < 30; i++) {
+                    // If we're done, break to get to resolve().
+                    if (uploads >= totalUploads) {
+                        break;
+                    }
+                    
+                    console.debug("Upload progress => " + uploads + "/" + totalUploads);
 
+                    // Wait 1 second to save CPU cycles.
+                    await delay(1000);
+                }
+        
+                // We're done uploading files.
+                resolve();
+            }).then(() => {
+                console.debug("File uploads handled!");
+
+                // Insert into the database via mutation.
+                categoryMut.mutate({
+                    parent_id: values.parent_id,
+                    name: values.name,
+                    name_short: values.name_short,
+                    url: values.url,
+                    classes: values.classes,
+                    icon: iconData?.toString() ?? null,
+                    iremove: values.iremove,
+                });
+            });
         }
     });
 
     return (
         <>
+            <FormikProvider value={form}>
+                <form method="POST" onSubmit={form.handleSubmit}>
+                    <div className="mb-4">
+                        <label className="block text-gray-200 text-sm font-bold mb-2">Image</label>
+                        <input className="shadow appearance-none border-blue-900 rounded w-full py-2 px-3 text-gray-200 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="image" name="image" type="file" placeholder="Source Image" onChange={(e) => {
+                            setIcon(e.currentTarget.files[0]);
+                        }} />
 
+                        <Field className="inline align-middle border-blue-900 rounded py-2 px-3 text-gray-200 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="image-remove" name="iremove" type="checkbox" /> <label className="inline align-middle text-gray-200 text-sm font-bold mb-2">Remove Current</label>
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-200 text-sm font-bold mb-2">Image Banner</label>
+                        <input className="shadow appearance-none border-blue-900 rounded w-full py-2 px-3 text-gray-200 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="image_banner" name="image_banner" type="file" placeholder="Source Image Banner" onChange={(e) => {
+                            setBanner(e.currentTarget.files[0]);
+                        }} />
+
+                        <input className="inline align-middle border-blue-900 rounded py-2 px-3 text-gray-200 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="bremove" name="image_banner-remove" type="checkbox" /> <label className="inline align-middle text-gray-200 text-sm font-bold mb-2">Remove Current</label>
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-200 text-sm font-bold mb-2">Name</label>
+                        <Field className="shadow appearance-none border-blue-900 rounded w-full py-2 px-3 text-gray-200 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="name" name="name" type="text" placeholder="Source Name" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-200 text-sm font-bold mb-2">URL</label>
+                        <Field className="shadow appearance-none border-blue-900 rounded w-full py-2 px-3 text-gray-200 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="url" name="url" type="text" placeholder="moddingcommunity.com" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-gray-200 text-sm font-bold mb-2">Classes</label>
+                        <Field className="shadow appearance-none border-blue-900 rounded w-full py-2 px-3 text-gray-200 bg-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="classes" name="classes" type="text" placeholder="CSS Classes" />
+                    </div>
+
+                    <button type="submit" className="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mt-2">{preUrl == null ? "Add!" : "Edit!"}</button>
+                </form>
+            </FormikProvider>
         </>
     );
 };
--- src/server/trpc/router/category.ts
+++ src/server/trpc/router/category.ts
@@ -1,49 +1,163 @@
 import { z } from "zod";
-
 import { router, publicProcedure } from "../trpc";
 
-export const categoryRouter = router({
-    getCategory: publicProcedure.input(z.object({
-        id: z.number().nullable(),
-    })).query(({ ctx, input}) => {
-        let cat = null;
+import fs from 'fs';
+import FileType from '../../../utils/base64';
+import { TRPCError } from "@trpc/server"
 
-        // ID.
-        if (input.id != null) {
-            cat = ctx.prisma.category.findFirst({
+export const categoryRouter = router({
+    getCategory: publicProcedure
+        .input(z.object({
+            id: z.number(),
+        }))
+        .query(({ ctx, input}) => {
+            const cat = ctx.prisma.category.findFirst({
                 where: {
                     id: input.id
                 }
-            })
-        }
+            });
 
-        return cat;
-    }),
+            return cat;
+        }),
     addCategory: publicProcedure
-        .input(
-        z.object({
+        .input(z.object({
+            parent_id: z.number(),
             name: z.string(),
+            name_short: z.string(),
             url: z.string(),
             icon: z.string().nullable(),
-            banner: z.string().nullable(),
             classes: z.string().nullable(),
-        })
-        )
+            iremove: z.boolean().nullable(),
+            id: z.number().nullable()
+        }))
         .mutation(async ({ ctx, input }) => {
-        try {
-            await ctx.prisma.category.create({
-            data: {
-                name: input.name,
-                icon: input.icon ?? null,
-                banner: input.banner ?? null,
-                classes: input.classes ?? null,
-            },
-            });
-        } catch (error) {
-            console.log(error);
-        }
+            let cat = null;
+
+            try {
+                cat = await ctx.prisma.category.upsert({
+                    where: {
+                        id: input.id ?? 0
+                    },
+                    update: {
+                        parentId: input.parent_id ?? 0,
+                        name: input.name,
+                        name_short: input.name_short,
+                        url: input.url,
+                        classes: input.classes ?? null        
+                    },
+                    create: {
+                        parentId: input.parent_id ?? 0,
+                        name: input.name,
+                        name_short: input.name_short,
+                        url: input.url,
+                        classes: input.classes ?? null 
+                    }
+                });
+            } catch (error) {
+                console.error("Error creating or updating category.");
+                console.error(error);
+
+                throw new TRPCError({ 
+                    code: "CONFLICT",
+                    message: error
+                });
+            }
+
+            if (cat != null) {
+                // Let's now handle file uploads.
+                let iconPath = null;
+
+                if (input.icon != null && input.icon.length > 0) {
+                    const base64Data = input.icon.split(',')[1];
+
+                    if (base64Data != null) {
+                        // Retrieve file type.
+                        const fileExt = FileType(base64Data);
+
+                        // Make sure we don't have an unknown file type.
+                        if (fileExt != "unknown") {
+                            // Now let's compile our file name.
+                            const fileName = cat.id + "." + fileExt;
+
+                            // Set icon path.
+                            iconPath = "images/category/" + fileName;
+
+                            // Convert to binary from base64.
+                            const buffer = Buffer.from(base64Data, 'base64');
+
+                            // Write file to disk.
+                            try {
+                                fs.writeFileSync(process.env.PUBLIC_DIR + "/" + iconPath, buffer);
+                            } catch (error) {
+                                console.error("Error writing icon to disk.");
+                                console.error(error);
+
+                                throw new TRPCError({ 
+                                    code: "PARSE_ERROR",
+                                    message: error
+                                });
+                            }
+                        } else {
+                            console.error("Icon's file extension is unknown.");
+
+                            throw new TRPCError({ 
+                                code: "PARSE_ERROR",
+                                message: error
+                            });
+                        }
+                    } else {
+                        console.error("Parsing base64 data is null.");
+
+                        throw new TRPCError({ 
+                            code: "PARSE_ERROR",
+                            message: "Unable to process banner's Base64 data."
+                        });
+                    }
+                }
+
+                // If we have a file upload, update database.
+                if (iconPath != null || input.iremove) {
+                    // If we're removing the icon or banner, make sure our data is null before updating again.
+                    if (input.iremove) {
+                        iconPath = null;
+                    }
+
+                    try {
+                        await ctx.prisma.category.update({
+                            where: {
+                                id: cat.id
+                            },
+                            data: {
+                                icon: iconPath
+                            }
+                        })
+                    } catch (error) {
+                        console.error("Error updating category when adding icon and banner.");
+                        console.error(error);
+
+                        throw new TRPCError({ 
+                            code: "BAD_REQUEST",
+                            message: "Error updating category with icon and banner data. " + error
+                        });
+                    }
+                }                
+            }
         }),
-    getAllCategories: publicProcedure.query(({ ctx }) => {
-        return ctx.prisma.category.findMany();
-    })
+    getCategoriesByParent: publicProcedure
+        .input(z.object({
+            parent_id: z.number()
+        }))
+        .query(({ ctx, input }) => {
+            const cats = ctx.prisma.category.findMany({
+                where: {
+                    parentId: input.parent_id
+                }
+            });
+
+            return cats;
+        }),
+    getAllCategories: publicProcedure
+        .query(({ ctx }) => {
+            return ctx.prisma.category.findMany();
+        })
 });
