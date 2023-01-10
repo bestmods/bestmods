9dcfecb6d9418ca581b977cf189b94c80841e49c
Christian Deacon <gamemann@gflclan.com>
2023-01-10 13:14:51 -0600

    Increase body limit for file uploads.

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -4,6 +4,15 @@ import { env } from "../../../env/server.mjs";
 import { createContext } from "../../../server/trpc/context";
 import { appRouter } from "../../../server/trpc/router/_app";
 
+export const config = {
+  api: {
+    responseLimit: '8mb',
+    bodyParser: { 
+      sizeLimit: '8mb' 
+    }
+  },
+}
+
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
