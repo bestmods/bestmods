6890359495d9db59dd67175a9b454932488d7b4e
Christian Deacon <gamemann@gflclan.com>
2023-01-12 17:22:47 -0600

    Add user router for permission checks.

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -4,12 +4,14 @@ import { authRouter } from "./auth";
 import  { categoryRouter } from './category';
 import { sourceRouter } from "./source";
 import { modRouter } from "./mod";
+import { userRouter } from './user';
 
 export const appRouter = router({
   source: sourceRouter,
   category: categoryRouter,
   mod: modRouter,
   auth: authRouter,
+  user: userRouter
 });
 
 // export type definition of API
---
+++ src/server/trpc/router/user.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
+
+export const userRouter = router({
+    checkPermission: publicProcedure
+        .input(z.object({
+            name: z.string()
+        }))
+        .query( async ({ ctx, input }) => {
+            const results = await ctx.prisma.permissions.findFirst({
+                where: {
+                    perm: input.name,
+                    userId: ctx.session?.user?.id ?? ""
+                }
+            });
+            
+            if (results)
+                return true;
+
+            return false;
+        })
+});
