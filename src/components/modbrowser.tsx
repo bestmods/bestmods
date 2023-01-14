5e7af8836dac392d1e03acd873924040e4f09100
Christian Deacon <gamemann@gflclan.com>
2023-01-14 12:27:20 -0600

    Add statistics to card with upvote/downvote buttons.

--- src/components/modbrowser.tsx
+++ src/components/modbrowser.tsx
@@ -121,8 +121,28 @@ const ModRow: React.FC<ModRowArguments> = ({ mod }) => {
                     </div>
                 )}
                 <div className="grow"></div>
-                <div className="modStats">
+                <div className="modStats flex justify-between items-center">
+                    <div className="relative ml-2 w-1/5">
+                        <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_429_11085)"><path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/><path d="M2 12C3.60014 7.90264 7.33603 5 12 5C16.664 5 20.3999 7.90264 22 12C20.3999 16.0974 16.664 19 12 19C7.33603 19 3.60014 16.0974 2 12Z" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/></g><defs><clipPath id="clip0_429_11085"><rect width="24" height="24" fill="white"/></clipPath></defs></svg>
+                        <span className="text-white text-sm ml-1">{mod.total_views.toString()}</span>
+                    </div>
 
+                    <div className="relative w-3/5 flex text-center justify-center items-center">
+                        <div className="mr-1">
+                            <svg className="w-12 h-12 text-center" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_429_11251)"><path d="M7 10L12 15" stroke="#FFA574" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 15L17 10" stroke="#FFA574" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/></g><defs><clipPath id="clip0_429_11251"><rect width="24" height="24" fill="white"/></clipPath></defs></svg>
+                        </div>
+                        <div className="text-center">
+                            <span className="text-white font-bold text-4xl">{rating}</span>
+                        </div>
+                        <div className="ml-1">
+                            <svg className="w-12 h-12 text-center" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_429_11224)"><path d="M17 14L12 9" stroke="#60A5FA" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 9L7 14" stroke="#60A5FA" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/></g><defs><clipPath id="clip0_429_11224"><rect width="24" height="24" fill="white"/></clipPath></defs></svg>
+                        </div>
+                    </div>
+
+                    <div className="relative mr-2 w-1/5 flex justify-end items-end flex-col">
+                        <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 11L12 15M12 15L8 11M12 15V3M21 15V17C21 18.1046 20.1046 19 19 19H5C3.89543 19 3 18.1046 3 17V15" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>
+                        <span className="text-white text-sm mr-1">{mod.total_downloads.toString()}</span>
+                    </div>
                 </div>
                 <div className="modLinks justify-between flex text-center bg-cyan-900">
                     <a href={viewLink} className="text-white font-bold p-2 w-1/3">View</a>
@@ -131,7 +151,11 @@ const ModRow: React.FC<ModRowArguments> = ({ mod }) => {
                             <div className="relative p-2 w-1/3">
                                 <button id={"installerDropdownBtn" + mod.id}  onClick={(e) => {
                                     setInstallersMenuOpen(!installersMenuOpen);
-                                }} className="text-white font-bold" type="button">Install <svg className="w-4 h-4 text-center ml-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                                }} className="text-white font-bold" type="button">Install {!installersMenuOpen ? (
+                                    <svg className="w-4 h-4 text-center ml-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_429_11251)"><path d="M7 10L12 15" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 15L17 10" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/></g><defs><clipPath id="clip0_429_11251"><rect width="24" height="24" fill="white"/></clipPath></defs></svg>
+                                ) : (
+                                    <svg className="w-4 h-4 text-center ml-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0_429_11224)"><path d="M17 14L12 9" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/><path d="M12 9L7 14" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/></g><defs><clipPath id="clip0_429_11224"><rect width="24" height="24" fill="white"/></clipPath></defs></svg>
+                                )}</button>
             
                                 <ul id={"installerDropdownMenu" + mod.id} className={`absolute py-1 text-sm bg-teal-800 ${ installersMenuOpen ? "block" : "hidden" }`} aria-labelledby={"installerDropdownBtn" + mod.id}>
                                     {installers.map((i) => {
