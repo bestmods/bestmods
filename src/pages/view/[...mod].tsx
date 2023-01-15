342b5b4bf20166e422660f0a8dbde2ec7c44ffd3
Christian Deacon <gamemann@gflclan.com>
2023-01-15 12:52:43 -0600

    Make some progress on mod view.

--- src/pages/view/[...mod].tsx
+++ src/pages/view/[...mod].tsx
@@ -1,3 +1,5 @@
+import { BestModsPage} from '../../../components/main';
+
 import { Mod } from "@prisma/client";
 import { Field, Form, Formik } from "formik";
 import { type NextPage } from "next";
@@ -10,72 +12,29 @@ import { trpc } from "../../utils/trpc";
 
 import { useRouter } from 'next/router'
 
-export const FieldStyle =
-  "rounded bg-slate-300 text-3xl text-slate-800 shadow-md shadow-slate-400";
-
-const SearchBar = () => {
-  return (
-    <Formik
-      initialValues={{ search: "" }}
-      onSubmit={(e) => {
-        history.pushState(null, "", `?search=${e.search}`);
-      }}
-    >
-      <Form className="w-4/5">
-        <Field
-          className={`w-full px-4 py-3 text-3xl ${FieldStyle}`}
-          placeholder="Search mod or add by mod name"
-          type="text"
-          name="search"
-        />
-      </Form>
-    </Formik>
-  );
-};
-
 const Home: NextPage = () => {
   return (
     <>
-      <HeadInfo />
-      <main className="flex min-h-screen flex-col bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-          <div className="container mx-auto flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-              <h1 className="text-center text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-                  <span className="text-blue-400">B</span>est{" "}
-                  <span className="text-blue-400">M</span>ods
-              </h1>
-              <SearchBar />
-
-              <ModMain />
-          </div>
-      </main>
+      <BestModsPage
+        content={<MainContent></MainContent>}
+      ></BestModsPage>
     </>
   );
 };
 
-const ModMain: React.FC = () => {
-  const { asPath, route, query } = useRouter()
+const MainContent: React.FC = () => {
+  const { query } = useRouter()
   const modParam = (query.mod != null) ? query.mod[0] : null;
   const modView = (query.mod != null && query.mod[1] != null) ? query.mod[1] : 'overview';
 
-  let url = null;
-  let id = null;
-
-  if (modParam != null) {
-    if (isNaN(Number(modParam))) {
-      url = modParam;
-    } else {
-      id = Number(modParam);
-    }
+  if (modParam == null) {
+    return <><div>Mod not found.</div></>;
   }
 
-  const modQuery = trpc.mod.getMod.useQuery({url: url, id: id});
+  const modQuery = trpc.mod.getMod.useQuery({url: modParam});
   const mod = modQuery.data;
 
   if (mod != null) {
-    // Attempt to retrieve source.
-    const sourceQuery = trpc.source.getSource.useQuery({id: mod.sourceId, url: null});
-    const source = sourceQuery.data;
-
     // Attempt to retrieve category.
     const categoryQuery = trpc.category.getCategory.useQuery({id: mod.categoryId});
     const category = categoryQuery.data;
@@ -107,21 +66,6 @@ const ModMain: React.FC = () => {
     const installLink = "/view/" + modParam + "/install";
     const downloadsLink = "/view/" + modParam + "/downloads";
     const editLink = "/view/" + modParam + "/edit";
-
-    if (mod.image == null || mod.image.length < 1) {
-      if (source != null && source.banner != null) {
-        headerPath += "sources/" + source.banner;
-      } else {
-        headerPath += "mods/default.png";
-      }
-    } else {
-      headerPath += "mods/" + mod.image;
-    }
-
-    // If we have a valid source, change the original link.
-    if (source != null) {
-      origLink = "https://" + source.url + "/" + mod.url;
-    }
 
     return (
       <>
