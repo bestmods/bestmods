97f0af8d5abd1ba5f6755a599a6f32dccb0b9348
Christian Deacon <gamemann@gflclan.com>
2023-01-18 01:05:29 -0600

    Make more style adjustments.

--- src/pages/view/[...mod].tsx
+++ src/pages/view/[...mod].tsx
@@ -222,19 +222,25 @@ const ModDownloads: React.FC<{ mod: Mod }> = ({ mod }) => {
 
   const downloads = downloadsQuery.data ?? [];
 
+  const dlCnt: number = mod.totalDownloads ?? 0;
+
   return (
-    <div className="relative flex flex-wrap gap-6">
-      {downloads.map((dl: ModDownload) => {
-        return (
-          <a className="bg-cyan-500/50 hover:bg-cyan-600/50 rounded p-4 max-w-lg" href={dl.url} target="_blank">
-            <div key={dl.modId + dl.url} className="flex items-center">
-              <svg className="w-5 h-5" viewBox="0 0 512 512" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg"><path d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/></svg>
-              <span className="text-white font-lg ml-2">{dl.name}</span>
-            </div>
-          </a>
-        );
-      })}
-    </div>
+    <>
+      <h3>Downloads</h3>
+      <div className="relative flex flex-wrap gap-6">
+        {downloads.map((dl: ModDownload) => {
+          return (
+            <a key={mod.id + "-" + dl.url} className="bg-cyan-500/50 hover:bg-cyan-600/50 rounded p-4 max-w-lg" href={dl.url} target="_blank">
+              <div key={dl.modId + dl.url} className="flex items-center">
+                <svg className="w-5 h-5" viewBox="0 0 512 512" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg"><path d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"/></svg>
+                <span className="text-white font-lg ml-2">{dl.name}</span>
+              </div>
+            </a>
+          );
+        })}
+      </div>
+      <p className="text-sm mt-4">{dlCnt.toString()} Total Downloads</p>
+    </>
   );
 };
 
