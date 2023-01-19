54bb0252871e8e429afa0caceec7b3718fdfd452
Christian Deacon <gamemann@gflclan.com>
2023-01-18 22:45:46 -0600

    Add download increment.

--- src/server/trpc/router/mod_download.ts
+++ src/server/trpc/router/mod_download.ts
@@ -4,6 +4,29 @@ import { router, publicProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server"
 
 export const modDownloadRouter = router({
+    incModDownloadCnt: publicProcedure
+        .input(z.object({
+            url: z.string()
+        }))
+        .mutation(async ({ ctx, input }) => {
+            try {
+                await ctx.prisma.mod.update({
+                    where: {
+                        url: input.url
+                    },
+                    data: {
+                        totalDownloads: {
+                            increment: 1
+                        }
+                    }
+                });
+            } catch (error) {
+                throw new TRPCError({
+                    message: error,
+                    code: "BAD_REQUEST"
+                });
+            }
+        }),
     getModDownloads: publicProcedure
         .input(z.object({
             id: z.number()
