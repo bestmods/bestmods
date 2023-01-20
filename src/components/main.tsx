706fdadeea66fbc85f3d4054aa591843ce2cd1c4
Christian Deacon <gamemann@gflclan.com>
2023-01-19 22:39:05 -0600

    Add navbar to header component.

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -115,6 +115,13 @@ export const BestModsHeader: React.FC = () => {
             <a href="/"><span className="text-blue-400">B</span>est{" "}
             <span className="text-blue-400">M</span>ods</a>
         </h1>
+        <div className="flex flex-wrap text-gray-300 font-mono text-sm gap-5">
+            <a className="hover:text-white" href="https://github.com/orgs/bestmods/discussions/categories/feedback-ideas" target="_blank">Feedback</a>
+            <a className="hover:text-white" href="https://github.com/bestmods/roadmap/issues" target="_blank">Roadmap</a>
+            <a className="hover:text-white" href="https://github.com/BestMods/bestmods" target="_blank">Source Code</a>
+            <a className="hover:text-white" href="https://github.com/orgs/bestmods/discussions/2">Removals</a>
+            <a className="hover:text-white" href="https://moddingcommunity.com/" target="_blank">Community</a>
+        </div>
 
         <Formik
             initialValues={{ 
