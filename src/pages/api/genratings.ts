af14a496700d69520da05b3e44f080d9bcd278b0
Christian Deacon <gamemann@gflclan.com>
2023-01-24 23:35:45 -0600

    Set recounting back to false.

--- src/pages/api/genratings.ts
+++ src/pages/api/genratings.ts
@@ -102,6 +102,7 @@ const genRatings = async (req: NextApiRequest, res: NextApiResponse) => {
       },
 
       data: {
+        needsRecounting: false,
         ...(hour != null && {
           ratingHour: hour
         }),
