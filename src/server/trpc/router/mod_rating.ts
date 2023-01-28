b15885d33b5b4db846de6f566c803483d5be8753
Christian Deacon <gamemann@gflclan.com>
2023-01-27 21:54:18 -0600

    Require recounting when submitting a user rating.

--- src/server/trpc/router/mod_rating.ts
+++ src/server/trpc/router/mod_rating.ts
@@ -76,7 +76,27 @@ export const modRatingRouter = router({
                 throw new TRPCError({
                     code: "CONFLICT",
                     message: (typeof error == "string") ? error : ""
-                })
+                });
+            }
+
+            // Require recounting now.
+            try {
+                await ctx.prisma.mod.update({
+                    where: {
+                        id: input.modId
+                    },
+                    data: {
+                        needsRecounting: true
+                    }
+                });
+            } catch (error) {
+                console.error("Error requiring recount for mod ID #" + input.modId);
+                console.error(error);
+
+                throw new TRPCError({
+                    code: "CONFLICT",
+                    message: (typeof error == "string") ? error : ""
+                });
             }
         })
 });
