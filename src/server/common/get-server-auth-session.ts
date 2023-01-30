6f2c3e535fbc9b8b53730a89bcfa29ccfdbc90f5
Christian Deacon <gamemann@gflclan.com>
2023-01-29 19:45:54 -0600

    Use getServerSession() after update.

--- src/server/common/get-server-auth-session.ts
+++ src/server/common/get-server-auth-session.ts
@@ -1,5 +1,5 @@
 import { type GetServerSidePropsContext } from "next";
-import { unstable_getServerSession } from "next-auth";
+import { getServerSession, unstable_getServerSession } from "next-auth";
 
 import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
@@ -11,5 +11,5 @@ export const getServerAuthSession = async (ctx: {
   req: GetServerSidePropsContext["req"];
   res: GetServerSidePropsContext["res"];
 }) => {
-  return await unstable_getServerSession(ctx.req, ctx.res, authOptions);
+  return await getServerSession(ctx.req, ctx.res, authOptions);
 };
