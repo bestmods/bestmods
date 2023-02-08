c004c6c46dc7df4cd6ade594776026e4cda8bb8e
Christian Deacon <gamemann@gflclan.com>
2023-02-08 11:54:08 -0600

    Change road map link.

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -285,7 +285,7 @@ export const MainNavItems: React.FC<{classes?: string | null}> = ({ classes }) =
     return (
         <>
             <a rel="noreferrer" className={classes != null ? classes : "text-gray-300 hover:text-white"} href="https://github.com/orgs/bestmods/discussions/categories/feedback-ideas" target="_blank">Feedback</a>
-            <a rel="noreferrer" className={classes != null ? classes : "text-gray-300 hover:text-white"} href="https://github.com/bestmods/roadmap/issues" target="_blank">Roadmap</a>
+            <a rel="noreferrer" className={classes != null ? classes : "text-gray-300 hover:text-white"} href="https://github.com/bestmods/roadmap/milestones" target="_blank">Roadmap</a>
             <a rel="noreferrer" className={classes != null ? classes : "text-gray-300 hover:text-white"} href="https://github.com/BestMods/bestmods" target="_blank">Source Code</a>
             <a rel="noreferrer" className={classes != null ? classes : "text-gray-300 hover:text-white"} href="https://github.com/orgs/bestmods/discussions/2" target="_blank">Removals</a>
             <a rel="noreferrer" className={classes != null ? classes : "text-gray-300 hover:text-white"} href="https://moddingcommunity.com/" target="_blank">Community</a>
