fbb8e7f840fdca3ba97482a53eefcac36b7b0694
Christian Deacon <gamemann@gflclan.com>
2023-06-21 01:32:50 -0500

    Add middleware procedure for future in ensuring user owns specific mod.

--- src/server/trpc/trpc.ts
+++ src/server/trpc/trpc.ts
@@ -3,6 +3,8 @@ import superjson from "superjson";
 
 import { type Context } from "./context";
 
+import { z } from "zod";
+
 const t = initTRPC.context<Context>().create({
     transformer: superjson,
     errorFormatter({ shape }) {
@@ -54,7 +56,7 @@ const isContributor = t.middleware(async ({ ctx, next }) => {
 
     return next({
         ctx: {
-            session: { ...ctx.session, user: ctx.session.user },
+            session: { ...ctx.session, user: ctx.session.user }
         }
     })
 })
@@ -77,9 +79,59 @@ const isAdmin = t.middleware(async ({ ctx, next }) => {
 
     return next({
         ctx: {
-            session: { ...ctx.session, user: ctx.session.user },
+            session: { ...ctx.session, user: ctx.session.user }
         }
     })
 })
 
 export const adminProcedure = t.procedure.use(isAdmin);
+
+export const modInputSchema = z.object({
+    modId: z.string().nullable(),
+    id: z.string().nullable(),
+
+    userId: z.number()
+})
+
+const ownsMod = t.middleware(async ({ ctx, rawInput, next }) => {
+    if (!ctx.session?.user)
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // Parse input and get mod ID.
+    const res = modInputSchema.safeParse(rawInput);
+
+    if (!res.success)
+        throw new TRPCError({ code: "BAD_REQUEST" })
+
+    const input = res.data;
+
+    if (input.modId == null && input.id == null)
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    const modId = input.modId ?? input.id;
+
+    // Now retrieve our mod and compare to user ID.
+    const mod = ctx.prisma.mod.findFirst({
+        select: {
+            owner: true
+        },
+        where: {
+            ownerId: modId
+        }
+    });
+
+    if (!mod)
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // Are we valid?
+    if (mod.owner.id != input.userId)
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    return next({
+        ctx: {
+            session: { ...ctx.session, user: ctx.session.user }
+        }
+    })
+})
+
+export const ownsModProcedure = t.procedure.use(ownsMod);
