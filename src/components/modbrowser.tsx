4c70437b604a043ea41fe6f49530f7d0d6fe3092
Christian Deacon <christianmdeacon@gmail.com>
2023-06-21 04:12:55 -0400

    20230621 nomodfix (#10)
    * Use only one column when no mods found.
    
    * Grammar correction.

--- src/components/modbrowser.tsx
+++ src/components/modbrowser.tsx
@@ -330,7 +330,7 @@ const ModBrowser: React.FC<{ categories?: Array<number> | null, visible?: boolea
         <div className="mx-auto w-full sm:w-4/5">
             <InfiniteScroll
                 pageStart={0}
-                className={display + "View"}
+                className={display + `View${(items.length < 1) ? " !grid-cols-1 sm:!grid-cols-1" : ""}`}
                 loadMore={loadMore}
                 hasMore={requireItems}
                 loader={
@@ -373,7 +373,7 @@ const ModBrowser: React.FC<{ categories?: Array<number> | null, visible?: boolea
                                 })}
                             </table>
                         ) : (
-                            <p className="mods-not-found">No mods founds.</p>
+                            <p className="mods-not-found">No mods found.</p>
                         )}
                     </>
                 )}
