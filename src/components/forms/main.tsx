9135fc57a775a6a230b1afd976a9dd8ae35fec98
Christian Deacon <gamemann@gflclan.com>
2023-06-23 15:56:41 -0500

    Use children instead of parameter for form wrapper.

--- src/components/forms/main.tsx
+++ src/components/forms/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 import { FormikProvider } from "formik";
 
-const FormTemplate: React.FC<{ form: any, submitBtn: JSX.Element, content: JSX.Element }> = ({ form, submitBtn, content }) => {
+const FormTemplate: React.FC<{ form: any, submitBtn: JSX.Element, children: ReactNode }> = ({ form, submitBtn, children }) => {
     return (
         <>
             {form != null && (
                 <FormikProvider value={form}>
                     <form method="POST" onSubmit={form.handleSubmit} className="w-full flex flex-col bg-black bg-opacity-50 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                        {content}
+                        {children}
                         {submitBtn}
                     </form>
                 </FormikProvider>
