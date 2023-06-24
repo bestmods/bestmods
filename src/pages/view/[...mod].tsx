4e95ea094f90e4985509409e24cf9fd36c011146
Christian Deacon <gamemann@gflclan.com>
2023-06-24 16:47:09 -0500

    Oops, there's multiple className attributes.

--- src/pages/view/[...mod].tsx
+++ src/pages/view/[...mod].tsx
@@ -324,11 +324,11 @@ const ModDownloads: React.FC = () => {
             <div className="relative flex flex-wrap gap-6">
                 {downloads.map((dl: ModDownload) => {
                     return (
-                        <a className="mod-download-item" rel="noreferrer" key={mod.id + "-" + dl.url} onClick={() => {
+                        <a rel="noreferrer" key={mod.id + "-" + dl.url} onClick={() => {
                             modDownloadMut.mutate({
                                 url: mod.url
                             });
-                        }} className="bg-cyan-500/50 hover:bg-cyan-600/50 rounded p-4 max-w-lg !no-underline" href={dl.url} target="_blank">
+                        }} className="mod-download-item" href={dl.url} target="_blank">
                             <div key={dl.modId + dl.url} className="flex items-center">
                                 <svg className="w-5 h-5" viewBox="0 0 512 512" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg"><path d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z" /></svg>
                                 <span className="text-white font-lg ml-2">{dl.name}</span>
