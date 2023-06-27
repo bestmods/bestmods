4734b0cfa6b3350518b4cad533fa47662e8605bb
Christian Deacon <gamemann@gflclan.com>
2023-06-26 19:50:35 -0500

    Improve card styling with width and height.

--- src/components/modbrowser/grid_row.tsx
+++ src/components/modbrowser/grid_row.tsx
@@ -45,8 +45,9 @@ const GridRow: React.FC<{
             </div>
             <div className="modbrowser-grid-main">
                 <h3>{mod.name}</h3>
-                <p>{descShort.substring(0, 120)}{dots}</p>
+                <p>{descShort}</p>
             </div>
+            <div className="grow"></div>
             {catPar && (
                 <div className="modbrowser-grid-category">
                     <img src={catParIcon} alt="Category Icon" />
@@ -71,11 +72,10 @@ const GridRow: React.FC<{
                     </span>
                 </div>
             )}
-            <div className="grow"></div>
             <div className="modbrowser-grid-stats">
                 <div className="modbrowser-grid-stats-views">
                     <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g clipPath="url(#clip0_429_11085)"><path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" /><path d="M2 12C3.60014 7.90264 7.33603 5 12 5C16.664 5 20.3999 7.90264 22 12C20.3999 16.0974 16.664 19 12 19C7.33603 19 3.60014 16.0974 2 12Z" stroke="#FFFFFF" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" /></g><defs><clipPath id="clip0_429_11085"><rect width="24" height="24" fill="white" /></clipPath></defs></svg>
-                    <span className="text-white text-sm ml-1">{mod.totalViews.toString()}</span>
+                    <span className="text-white text-sm">{mod.totalViews.toString()}</span>
                 </div>
 
                 <ModRatingRender
