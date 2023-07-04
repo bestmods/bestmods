8fa11fbf0db81d037cd37af391dcbbe930e2dc83
Christian Deacon <gamemann@gflclan.com>
2023-07-04 12:53:00 -0500

    Update removals link.

--- src/components/main/header/nav_items.tsx
+++ src/components/main/header/nav_items.tsx
@@ -8,7 +8,7 @@ export const NavItems: React.FC<{
             <a rel="noreferrer" className={classes ? classes : "text-gray-300 hover:text-white"} href="https://github.com/orgs/bestmods/discussions/categories/feedback-ideas" target="_blank">Feedback</a>
             <a rel="noreferrer" className={classes ? classes : "text-gray-300 hover:text-white"} href="https://github.com/bestmods/bestmods/milestones" target="_blank">Roadmap</a>
             <a rel="noreferrer" className={classes ? classes : "text-gray-300 hover:text-white"} href="https://github.com/BestMods/bestmods" target="_blank">Source Code</a>
-            <a rel="noreferrer" className={classes ? classes : "text-gray-300 hover:text-white"} href="https://github.com/orgs/bestmods/discussions/2" target="_blank">Removals</a>
+            <a rel="noreferrer" className={classes ? classes : "text-gray-300 hover:text-white"} href="https://github.com/orgs/bestmods/discussions/35" target="_blank">Removals</a>
             <a rel="noreferrer" className={classes ? classes : "text-gray-300 hover:text-white"} href="https://moddingcommunity.com/" target="_blank">Community</a>
         </>
     );
