5fb83eb0888239786681274c138add5b59004e80
Christian Deacon <gamemann@gflclan.com>
2023-12-05 02:30:30 -0600

    Improve background component and remove background image by default.

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -2,20 +2,20 @@ import { useContext, useEffect } from "react"
 import { ViewPortCtx } from "./main";
 
 export default function Background ({
-    image = null,
+    image,
     overlay = true 
 }: {
-    image?: string | null,
-    overlay?: boolean | string
+    image?: string,
+    overlay?: boolean
 }) {
     const viewPort = useContext(ViewPortCtx);
 
-    return (<>
+    return (
         <div
-            className={`top-0 left-0 w-full h-full -z-50 fixed bg-fixed bg-center bg-no-repeat bg-cover bg-gradient-to-b from-[#002736] to-[#00151b]${(overlay && !viewPort.isMobile) ? ` brightness-[20%]` : ``}`}
+            className={`top-0 left-0 w-full h-full -z-50 fixed bg-fixed bg-center bg-no-repeat bg-cover bg-gradient-to-b from-[#002736] to-[#00151b]${(image && overlay && !viewPort.isMobile) ? ` brightness-[20%]` : ``}`}
             style={{
                 backgroundImage: (!viewPort.isMobile && image) ? `url('${image}')` : undefined
             }}
         ></div>
-    </>)
+    )
 }
