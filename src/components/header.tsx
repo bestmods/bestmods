403f01881455f68496e28697e8146244938866f1
Christian Deacon <gamemann@gflclan.com>
2023-12-05 02:30:09 -0600

    Make progress on header.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,6 +18,7 @@ import { ViewPortCtx } from "./main";
 import HamburgerIcon from "./icons/hamburger";
 import LeftArrowIcon from "./icons/left_arrow";
 import HomeIcon from "./icons/home";
+import SteamIcon from "./icons/steam";
 
 export default function Header () {
     const router = useRouter();
@@ -40,7 +41,7 @@ export default function Header () {
                     >
                         <HamburgerIcon className="w-6 h-6 stroke-white" />
                     </button>
-                    <div className={`${mobileOpen ? "block" : "hidden"} fixed z-30 top-0 left-0 w-1/2 bg-bestmods-2 flex justify-between overflow-y-scroll`}>
+                    <div className={`${mobileOpen ? "block" : "hidden"} fixed z-30 top-0 left-0 w-1/2 bg-bestmods-2 flex justify-between overflow-y-scroll p-4`}>
                         <div className="grow flex flex-col gap-4">
                             <Link
                                 href="/"
@@ -66,14 +67,32 @@ export default function Header () {
                                     text={<>Categories</>}
                                 />
                             </Link>
-                            <h2>Socials</h2>
+                            <h3>Socials</h3>
+                            <Link
+                                href="https://instagram.com/bestmodsio"
+                                target="_blank"
+                            >
+                                <IconAndText
+                                    icon={<InstagramIcon className="w-6 h-6 fill-white" />}
+                                    text={<>Instagram</>}
+                                />
+                            </Link>
                             <Link
                                 href="https://twitter.com/bestmodsio"
                                 target="_blank"
                             >
                                 <IconAndText
-                                    icon={<TwitterIcon className="w-6 h-6 stroke-white" />}
-                                    text={<>Twitter</>}
+                                    icon={<TwitterIcon className="w-6 h-6 fill-white" />}
+                                    text={<>Twitter/X</>}
+                                />
+                            </Link>
+                            <Link
+                                href="https://steamcommunity.com/groups/best-mods"
+                                target="_blank"
+                            >
+                                <IconAndText
+                                    icon={<SteamIcon className="w-6 h-6 fill-white" />}
+                                    text={<>Steam</>}
                                 />
                             </Link>
                             <Link
@@ -81,20 +100,11 @@ export default function Header () {
                                 target="_blank"
                             >
                                 <IconAndText
-                                    icon={<FacebookIcon className="w-6 h-6 stroke-white" />}
+                                    icon={<FacebookIcon className="w-6 h-6 fill-white" />}
                                     text={<>Facebook</>}
                                 />
                             </Link>
-                            <Link
-                                href="https://instagram.com/bestmodsio"
-                                target="_blank"
-                            >
-                                <IconAndText
-                                    icon={<InstagramIcon className="w-6 h-6 stroke-white" />}
-                                    text={<>Instagram</>}
-                                />
-                            </Link>
-                            <h2>More</h2>
+                            <h3>More</h3>
                             <Link
                                 href="https://github.com/orgs/bestmods/discussions/categories/feedback-idea"
                                 target="_blank"
@@ -102,6 +112,42 @@ export default function Header () {
                                 <IconAndText
                                     icon={<ChatBubbleIcon className="w-6 h-6 stroke-white" />}
                                     text={<>Feedback</>}
+                                />
+                            </Link>
+                            <Link
+                                href="https://github.com/bestmods/bestmods/milestones"
+                                target="_blank"
+                            >
+                                <IconAndText
+                                    icon={<MapIcon className="w-6 h-6 stroke-white" />}
+                                    text={<>Roadmap</>}
+                                />
+                            </Link>
+                            <Link
+                                href="https://github.com/BestMods/bestmods"
+                                target="_blank"
+                            >
+                                <IconAndText
+                                    icon={<CodeIcon className="w-6 h-6 stroke-white" />}
+                                    text={<>Source Code</>}
+                                />
+                            </Link>
+                            <Link
+                                href="https://github.com/orgs/bestmods/discussions/35"
+                                target="_blank"
+                            >
+                                <IconAndText
+                                    icon={<TrashIcon className="w-6 h-6 stroke-white" />}
+                                    text={<>Removals</>}
+                                />
+                            </Link>
+                            <Link
+                                href="https://moddingcommunity.com"
+                                target="_blank"
+                            >
+                                <IconAndText
+                                    icon={<UsersIcon className="w-6 h-6 stroke-white" />}
+                                    text={<>Community</>}
                                 />
                             </Link>
                         </div>
@@ -149,14 +195,34 @@ export default function Header () {
                         active={cur == "/category"}
                     />
                     <DropDown
-                        html={<>Social</>}
+                        html={<>Socials</>}
                         drop_down_items={[
+                            {
+                                link: "https://instagram.com/bestmodsio",
+                                html:
+                                    <IconAndText
+                                        icon={<InstagramIcon className="w-6 h-6 fill-white" />}
+                                        text={<>Instagram</>}
+                                    />
+                                ,
+                                new_tab: true
+                            },
                             {
                                 link: "https://twitter.com/bestmodsio",
                                 html:
                                     <IconAndText
                                         icon={<TwitterIcon className="w-6 h-6 fill-white" />}
-                                        text={<>Twitter</>}
+                                        text={<>Twitter/X</>}
+                                    />
+                                ,
+                                new_tab: true
+                            },
+                            {
+                                link: "https://steamcommunity.com/groups/best-mods",
+                                html:
+                                    <IconAndText
+                                        icon={<SteamIcon className="w-6 h-6 fill-white" />}
+                                        text={<>Steam</>}
                                     />
                                 ,
                                 new_tab: true
@@ -167,16 +233,6 @@ export default function Header () {
                                     <IconAndText
                                         icon={<FacebookIcon className="w-6 h-6 fill-white" />}
                                         text={<>Facebook</>}
-                                    />
-                                ,
-                                new_tab: true
-                            },
-                            {
-                                link: "https://instagram.com/bestmodsio",
-                                html:
-                                    <IconAndText
-                                        icon={<InstagramIcon className="w-6 h-6 fill-white" />}
-                                        text={<>Instagram</>}
                                     />
                                 ,
                                 new_tab: true
