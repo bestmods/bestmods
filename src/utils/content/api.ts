b15dd09e2c532e2db22c15e4ede010fc2a0e47e0
Christian Deacon <gamemann@gflclan.com>
2023-12-11 22:31:32 -0600

    Create util function to check for API access.

---
+++ src/utils/content/api.ts
@@ -0,0 +1,60 @@
+import { type NextApiRequest } from "next";
+
+import { prisma } from "@server/db/client";
+
+export async function CheckApiAccess({
+    req,
+    token
+} : {
+    req: NextApiRequest
+    token: string
+}): Promise<[boolean, string | null]> {
+    // Attempt to retrieve key from database and check.
+    const apiKey = await prisma.apiKey.findFirst({
+        where: {
+            key: token
+        }
+    });
+
+    if (!apiKey)
+        return [false, "No key found in database."];
+
+    // IP check.
+    const ipAddr = req.socket.remoteAddress;
+
+    if (apiKey.ipAddr && ipAddr !== apiKey.ipAddr)
+        return [false, "IP address not allowed."];
+
+    // User agent check.
+    const agent = req.headers?.["user-agent"];
+
+    if (apiKey.agent && agent !== apiKey.agent)
+        return [false, "User agent not allowed."];
+
+    // Endpoint check.
+    const endPoint = req.url;
+
+    if (apiKey.endPoint && endPoint !== apiKey.endPoint)
+        return [false, "Endpoint not allowed."];
+
+    // Rate limit check.
+    const rateLimit = apiKey.rateLimit;
+
+    if (rateLimit) {
+        const hourAgo = new Date(Date.now() - 3600);
+
+        const hits = await prisma.apiLog.count({
+            where: {
+                apiKeyId: apiKey.id,
+                createdAt: {
+                    gte: hourAgo
+                }
+            }
+        })
+
+        if (hits > rateLimit)
+            return [false, `Rate limit exceeded (${hits.toString()} > ${rateLimit.toString()}).`]
+    }
+
+    return [true, null];
+}
