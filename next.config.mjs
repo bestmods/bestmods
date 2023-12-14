ecd8f0cc33d9450cdf753471b8e08d451ed0ba4c
Christian Deacon <gamemann@gflclan.com>
2023-12-14 03:39:21 -0600

    Enable any https Next image URLs.

--- next.config.mjs
+++ next.config.mjs
@@ -13,5 +13,15 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: '**',
+            port: '',
+            pathname: '**',
+        }
+    ]
+  }
 };
 export default config;
