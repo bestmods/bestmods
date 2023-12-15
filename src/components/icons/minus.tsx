dd44922cacd4458902cda02b37ffca3352753114
Christian Deacon <gamemann@gflclan.com>
2023-12-15 02:17:44 -0600

    Add more icons.

---
+++ src/components/icons/minus.tsx
@@ -0,0 +1,11 @@
+export default function MinusIcon ({
+    className
+} : {
+    className?: string
+}) {
+    return (
+        <svg className={className} viewBox="0 0 448 512">
+            <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
+        </svg>
+    )
+}
---
+++ src/components/icons/plus.tsx
@@ -0,0 +1,11 @@
+export default function PlusIcon ({
+    className
+} : {
+    className?: string
+}) {
+    return (
+        <svg className={className} viewBox="0 0 448 512">
+            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+        </svg>
+    )
+}
