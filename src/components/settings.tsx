521d7880b89a96cb03ebc0afc4b74c961aa5db10
Christian Deacon <gamemann@gflclan.com>
2023-12-16 15:04:20 -0600

    Add different icons to login/logout in settings menu and add animation by setting static heights.

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -4,6 +4,8 @@ import { useCookies } from "react-cookie";
 import PhotoIcon from "./icons/photo";
 import UserIcon from "./icons/user";
 import GearIcon from "./icons/gear";
+import RightToBracketIcon from "./icons/right_to_bracket";
+import ExitIcon from "./icons/exit";
 
 export default function Settings ({
     showBg,
@@ -19,9 +21,9 @@ export default function Settings ({
     const [,setCookie] = useCookies(["bm_showbg"]);
 
     return (
-        <div className="fixed z-20 bottom-0 left-0 p-4 duration-300 bg-bestmods-3 flex flex-col justify-center items-center gap-8 group rounded-tr">
+        <div className={`${showMenu ? "h-[11rem]" : "h-14"} overflow-hidden fixed z-20 bottom-0 left-0 px-4 py-2 transition-all duration-300 bg-bestmods-3 flex flex-col justify-end gap-8 rounded-tr`}>
             <button
-                className={showMenu ? "block cursor-pointer" : "hidden"}
+                className="cursor-pointer"
                 onClick={() => {
                     setShowBg(!showBg);
                     setCookie("bm_showbg", !showBg ? "1" : "0");
@@ -30,7 +32,7 @@ export default function Settings ({
                 <PhotoIcon className={`w-8 h-8 rounded-full stroke-white fill-none ${showBg ? "brightness-100" : "brightness-50"}`} />
             </button>
             <button
-                className={showMenu ? "block cursor-pointer" : "hidden"}
+                className="cursor-pointer"
                 onClick={async () => {
                     if (session?.user)
                         await signOut();
@@ -38,7 +40,11 @@ export default function Settings ({
                         await signIn("discord");
                 }}
             >
-                <UserIcon className="w-8 h-8 stroke-white fill-none" />
+                {session?.user ? (
+                    <ExitIcon className="w-8 h-8 fill-red-400" />
+                ) : (
+                    <RightToBracketIcon className="w-8 h-8 fill-white" />
+                )}
             </button>
             <div
                 className="cursor-pointer"
