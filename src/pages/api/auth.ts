7d8ab743505585cbb2fa2e4626ca519290837d69
Christian Deacon <gamemann@gflclan.com>
2023-12-16 15:56:39 -0600

    Start working on auth API endpoint.

---
+++ src/pages/api/auth.ts
@@ -0,0 +1,23 @@
+import { CheckApiAccess } from "@utils/api";
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+export default async function Auth (req: NextApiRequest, res: NextApiResponse) {
+    const key = process.env.API_AUTH_KEY ?? "";
+
+    // Check if we have access.
+    const [ret, err, method] = await CheckApiAccess({
+        req: req,
+        key: key,
+        methods: ["POST"]
+    })
+
+    if (ret !== 200) {
+        return res.status(ret).json({
+            message: err
+        });
+    }
+
+    // Check if we should update auth.
+
+
+}
