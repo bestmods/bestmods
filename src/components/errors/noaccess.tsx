2d333585872d3967b7877a7167b8182f0856336b
Christian Deacon <gamemann@gflclan.com>
2024-01-16 10:50:14 -0600

    Add more details to no access component.

--- src/components/errors/noaccess.tsx
+++ src/components/errors/noaccess.tsx
@@ -1,9 +1,14 @@
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+
 export default function NoAccess() {
     return (
         <div className="flex flex-col gap-2">
             <h1>No Permissions!</h1>
             <div className="bg-bestmods-2/80 rounded p-4">
-                <p>You do not have access to view this page. If this is a mistake, please contact an administrator.</p>
+                <p>You do not have access to view this page. Please make sure you{"'"}re signed in and have access to view this page. You may click <Link href="#" onClick={() => void signIn("discord")}>here</Link> to sign in.</p>
+                    
+                <p>If you{"'"}re still having issues, please contact an administrator.</p>
             </div>
         </div>
     )
