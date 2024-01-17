f69bc845d0b20571153119d590402f87015534df
Christian Deacon <gamemann@gflclan.com>
2024-01-17 02:51:03 -0600

    Create function for add unique mod view.

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,7 +1,29 @@
+import { type Session } from "next-auth";
 import { type ModWithCategory, type ModRowBrowser, type ModViewItem } from "~/types/mod";
+import { type PrismaClient } from "@prisma/client";
 
 export function GetModUrl(mod: ModViewItem | ModRowBrowser | ModWithCategory ) {
     const catUrl = mod.category?.parent?.url ?? mod.category?.url ?? "global";
 
     return `/${catUrl}/mod/${mod.url}`;
+}
+
+export async function InsertUniqueView(prisma: PrismaClient, mod: ModViewItem, session?: Session) {
+    if (!session?.user)
+        return;
+
+    // Insert unique mod view if we're signed in.
+    await prisma.modUniqueView.upsert({
+        where: {
+            modId_userId: {
+                userId: session.user.id,
+                modId: mod.id
+            }
+        },
+        create: {
+            userId: session.user.id,
+            modId: mod.id
+        },
+        update: {}
+    })
 }
