10d25390cae2e1420ce0bfcea90a89b50de0033a
Christian Deacon <christianmdeacon@gmail.com>
2024-03-24 13:32:13 -0400

    Improve 404 not found page.

--- src/components/errors/notfound.tsx
+++ src/components/errors/notfound.tsx
@@ -1,13 +1,18 @@
+import Link from "next/link";
+
 export default function NotFound ({
     item = "page"
 } : {
     item?: string
 }) {
+    const itemName = item.charAt(0).toLowerCase() + item.slice(1);
+    
     return (
         <div className="flex flex-col gap-2">
             <h2>Not Found!</h2>
             <div className="bg-bestmods-2/80 rounded p-4">
-                <p>The {item.charAt(0).toLowerCase() + item.slice(1)} you{"'"}ve requested was not found! Please check the URL and make sure it is correct.</p>
+                <p>The {itemName} you{"'"}ve requested was not found! It's possible the {item} was removed from our database or the URL is incorrect.</p>
+                <p>Discover mods <Link href="https://bestmods.io/browse">here</Link>!</p>
             </div>
         </div>
     )
